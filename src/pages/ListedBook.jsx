0b7d4274c504eb9c7dfd5658c4a291da3ec7ba1b
YeasinWebDev <ar9485727@gmail.com>
2024-03-27 11:48:24 +0600

    sort method

--- src/pages/ListedBook.jsx
+++ src/pages/ListedBook.jsx
@@ -7,7 +7,27 @@ function ListedBook() {
   const [readData, setReadData] = useState([]);
   const [wishData, setWishData] = useState([]);
 
-  
+  const handleSort = (name) => {
+    if (name === 'Rating') {
+      const sortedData = [...readData].sort((a, b) => b.rating - a.rating);
+      const sortedDataWish = [...wishData].sort((a, b) => b.rating - a.rating);
+      setReadData(sortedData);
+      setWishData(sortedDataWish);
+    }
+    else if(name === 'pages'){
+      const sortedData = [...readData].sort((a, b) => b.totalPages - a.totalPages);
+      const sortedDataWish = [...wishData].sort((a, b) => b.totalPages - a.totalPages);
+      setReadData(sortedData);
+      setWishData(sortedDataWish);
+    }
+    else {
+      const sortedData = [...readData].sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+      const sortedDataWish = [...wishData].sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+      setReadData(sortedData);
+      setWishData(sortedDataWish);
+    }
+
+  }
 
 
   useEffect(() => {
@@ -32,9 +52,9 @@ function ListedBook() {
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn m-1 bg-[#23be0a] border-none hover:bg-[#23be0a] text-white">Sort By</div>
           <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-[#f3f3f3] text-black rounded-box w-52">
-            <li ><a> Rating </a></li>
-            <li><a> Number of pages </a></li>
-            <li><a> Publisher year </a></li>
+            <li onClick={() => handleSort("Rating")}><a> Rating </a></li>
+            <li onClick={() => handleSort("pages")}><a> Number of pages </a></li>
+            <li onClick={() => handleSort("year")}><a> Publisher year </a></li>
           </ul>
         </div>
       </div>
